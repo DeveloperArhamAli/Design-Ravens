72a65b28a9902a632ecd7a9b5809bf7044079a60
Muhammad Arham Ali Khan <muhammadarhamalikhan2009@gmai.com>
2025-05-05 19:00:29 +0500

    updated files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#050816] text-white">
         <div className="text-purple-200 flex items-center justify-center py-4 z-50 text-center px-2.5">
-          <p>Copyright &copy; {new Date().getFullYear()} by <Link to="/" className="text-cyan-400 font-bolder">Rare Teck</Link>. All rights reserved | Powered by <a href="https://aura-enterprises.infinityfreeapp.com" target="_blank" className="text-cyan-400 font-bolder">Aura Enterprises</a></p>
+          <p>Copyright &copy; {new Date().getFullYear()} by <Link to="/" className="text-cyan-400 font-bolder">Rare Teck</Link>. All rights reserved.</p>
       </div>
     </footer>
   )
